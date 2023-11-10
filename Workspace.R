f834c257f599feb0abbe75a62d09369c9d2b9aae
Michael Nowell <michaelnowell84@yahoo.com>
2023-11-10 19:11:47 +0800

    Basic Cleaning of Data - 1

--- Workspace.R
+++ Workspace.R
@@ -1,6 +1,29 @@
+##Set WD and Load Library
 getwd()
-setwd("C:/Users/michael/Documents/Progressive Overload Data")
+setwd("~/Progressive-Overload-Data")
 library(readr)
 library(tidyverse)
 library(ggplot2)
+library(lubridate)
+
+##Load Data
 fulldata <- read.csv("workoutlog.csv")
+View(fulldata)
+
+##Remove Notes Column because it has no data
+fulldata <- select(fulldata, -(Notes))
+View(fulldata)
+
+##Transform Date from Character to Date format
+class(fulldata$Date)
+fulldata$Date <- mdy(fulldata$Date)
+
+##Check change
+class(fulldata$Date)
+
+##Sort by Date
+date_sorted <- arrange(fulldata,Date)
+View(date_sorted)
+
+
+
