0b2b9d0df35977c4e5bb51ef346c7cd050c387ec
Michael Nowell <michaelnowell84@yahoo.com>
2023-11-11 18:01:33 +0800

    Begin computation of total volumes and exercises

--- Workspace.R
+++ Workspace.R
@@ -14,7 +14,10 @@ View(fulldata)
 fulldata <- select(fulldata, -(Notes))
 View(fulldata)
 
-##Transform Date from Character to Date format
+##Change X...of Reps column to just be 'Reps' for easier naming convention
+colnames(fulldata)[4] <- ('Reps')
+
+##Transform Date Column from Character to Date format
 class(fulldata$Date)
 fulldata$Date <- mdy(fulldata$Date)
 
@@ -25,5 +28,34 @@ class(fulldata$Date)
 date_sorted <- arrange(fulldata,Date)
 View(date_sorted)
 
+##How many unique dates / number of workouts = 127
+n_distinct(fulldata$Date)
+
+##How many unique exercises = 21
+n_distinct(fulldata$Exercise)
+
+##Check for NA values = 0
+sum(is.na(fulldata))
+
+##Sets per Exercise sorted by highest to lowest and View results
+sets_per_exercise <- table(fulldata$Exercise) %>% 
+  sort(sets_per_exercise, decreasing = TRUE)
+View(sets_per_exercise)
+
+##Change to DF and alter Column names for better understanding
+sets_per_exercse <- as.data.frame(sets_per_exercise)
+colnames(sets_per_exercse) <- c('Exercise','Total Sets')
+View(sets_per_exercise)
+
+##Calculate Total Volume in lbs
+total_volume <- date_sorted %>% 
+  mutate(Total_Volume = Reps*Weight)
+View(total_volume)
+
+##Find how much volume per exercise per day, sort by date then desc volume
+exercise_vol_by_day <- total_volume %>% 
+  group_by(Date, Exercise) %>% 
+  summarize(Sum_Total_Vol_LBs = sum(Total_Volume)) %>% 
+  arrange(Date, desc(Sum_Total_Vol_LBs))
 
 
