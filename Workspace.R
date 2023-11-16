b8ef096f189494b10888da96c9991c9e6cbabb4f
Michael Nowell <michaelnowell84@yahoo.com>
2023-11-16 17:46:18 +0800

    Sets and reps per day calculated and tabled and checked for accuracy

--- Workspace.R
+++ Workspace.R
@@ -16,6 +16,7 @@ View(fulldata)
 
 ##Change X...of Reps column to just be 'Reps' for easier naming convention
 colnames(fulldata)[4] <- ('Reps')
+View(fulldata)
 
 ##Transform Date Column from Character to Date format
 class(fulldata$Date)
@@ -55,8 +56,9 @@ View(total_volume)
 ##Find how much volume per exercise per day, sort by date then desc volume
 exercise_vol_by_day <- total_volume %>% 
   group_by(Date, Exercise) %>% 
-  summarize(Sum_Total_Vol_LBs = sum(Total_Volume)) %>% 
+  summarize(Sum_Total_Vol_LBs = sum(Total_Volume)) %>%
   arrange(Date, desc(Sum_Total_Vol_LBs))
+View(exercise_vol_by_day)
 
 ##Chart 1 - Sets per Exercise
 sets_per_exercse %>% 
@@ -65,3 +67,26 @@ sets_per_exercse %>%
   theme(axis.text.x = element_text(angle = 60, vjust = 1.0, hjust=1))
 
 
+##Reps per day per exercise and arrange by Date and Reps
+reps_per_day <- total_volume %>% 
+  group_by(Date, Exercise) %>% 
+  summarize(Total_Reps = sum(Reps)) %>% 
+  arrange(Date, desc(Total_Reps))
+
+View(reps_per_day)
+
+
+##Sets per day, rename column from 'n' to 'Sets', and arrange by Date and Sets
+sets_per_day <- total_volume %>% 
+  group_by(Date) %>% 
+  count(Exercise) %>% 
+  rename(Sets = n) %>% 
+  arrange(Date, desc(Sets))
+  
+View(sets_per_day)
+
+  
+
+
+##Need to chart sets, reps, and total volume by date and wrap by exercise 
+##so you can see progress of each movement over time
